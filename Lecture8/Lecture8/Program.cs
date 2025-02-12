d45f81d0fad50e2cf01afc54ae467a81693f5c7c
Admiral General <muhammadusmanawan88@gmail.com>
2025-02-12 20:28:20 +0500

    lecture8 start on vs

---
+++ Lecture8/Lecture8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lecture8
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
