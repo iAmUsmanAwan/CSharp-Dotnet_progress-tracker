79b1d10adca1d973c5fc6fd02c2cb05c51429e7e
Admiral General <muhammadusmanawan88@gmail.com>
2025-02-24 04:20:33 +0500

    Lecture10 done

---
+++ Lecture10/Lecture10/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture10
+{
+    //class TextAndNumber
+    //{
+    //    public string Text;
+    //    public int Number;
+    
+    //}
+
+    class Person
+    {
+        public string Name;
+        public int Age;
+        public string Country;
+
+        public void PrintPersonDetails()
+        {
+            Console.WriteLine($"Person name is: {this.Name} " + $" and age is: {this.Age}" );
+        }
+
+        public string GetPersonName()
+        {
+            return $"My name is {this.Name}" ; 
+        }
+
+        //public TextAndNumber GetPersonNameAndAge()
+        //{
+        //    return new TextAndNumber { Text=this.Name, Number=this.Age };
+        //}
+
+        //public (string, int) GetPersonDetails()    // here we have fetched both the data without creating new class 
+        //{
+        //    return(this.Name, this.Age);
+        //}
+
+        public (string MyName, int MyAge, string MyCountry) GetPersonDetails()     
+        {
+            return (this.Name, this.Age, this.Country);
+        }
+
+    }
+}
---
+++ Lecture10/Lecture10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lecture10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person { Age = 12, Name = "Ali", Country = "Pakistan" }; 
+            
+            p.PrintPersonDetails();
+            Console.WriteLine(p.GetPersonName());
+
+            //Console.WriteLine("The name is " + p.GetPersonNameAndAge().Text + " and age is " + p.GetPersonNameAndAge().Number );
+
+            //(string, int) personData = p.GetPersonDetails();
+            //Console.WriteLine(personData.Item1);
+            //Console.WriteLine(personData.Item2);
+
+            var personData = p.GetPersonDetails();
+            Console.WriteLine(personData.MyName);
+            Console.WriteLine(personData.MyAge);
+            Console.WriteLine(personData.MyCountry);
+
+            (string n, int a, string c) personData2 = p.GetPersonDetails();
+            Console.WriteLine(personData2.n);
+            Console.WriteLine(personData2.a);
+            Console.WriteLine(personData2.c);
+
+            (string name, int age, string country) = p.GetPersonDetails();
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(country);
+
+        }
+    }
+}
