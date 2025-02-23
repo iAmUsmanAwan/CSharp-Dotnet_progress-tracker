955cc667291acd512aad8b16953e22bd2dfa21d4
Admiral General <muhammadusmanawan88@gmail.com>
2025-02-23 12:15:59 +0500

    Lecture9 done

---
+++ Lecture9/Lecture9/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture9
+{
+    public class Person  : System.Object  // this is inherited from 'system.object' always even if we dont write it
+    {
+        public string Name;
+        public int Age;
+        public static string Country = "Pakistan";
+        public const string HomePlanet = "Earth";   // const cannot be changed, unlike the static
+        public readonly string HomePlanet2;
+
+        public void Calculate(int Z)
+        {
+            const int X = 10, Y = 20;
+            const int W = X + Y;
+            //const int R = X + Z;  // this cannot be assigned because it is assigned during the runtime, this will be catered by readonly 
+        }
+
+        public Person() {
+            Name = default;
+            Age = default;
+            HomePlanet2 = "Mooon";
+        }
+
+        public Person(string name, int age) { 
+            this.Name = name;
+            this.Age = age;
+            HomePlanet2 = "Mooon";
+
+        }
+
+    }
+}
---
+++ Lecture9/Lecture9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Lecture9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personObj = new Person("Ali", 13);
+            Console.WriteLine(personObj.Name);
+            Console.WriteLine(personObj.Age);
+            Console.WriteLine(Person.Country);   // Static data is shared data, it will be accessed by the class not the object
+            Console.WriteLine(Person.HomePlanet);
+            Console.WriteLine(personObj.HomePlanet2);
+            //personObj.HomePlanet2 = "Sun";   // readonly fields cannot be reassigned
+
+
+            Person P2 = new Person(age: 14, name: "Ibrahim");   // here when we are using named arguments, then position does not matter
+            Console.WriteLine(P2.Name);
+            Console.WriteLine(P2.Age);
+            Console.WriteLine(Person.Country);
+
+            personObj.Name = "Mustafa";
+            Console.WriteLine(personObj.Name);
+
+            Person.Country = "China";   // This will be changed for the whole class not just one object
+
+            Person P3 = new Person() { Age= 23, Name= "Musa"};
+            Console.WriteLine(P3.Name);
+            Console.WriteLine(P3.Age);
+            Console.WriteLine(Person.Country);
+
+            Console.WriteLine(P2.Name);
+            Console.WriteLine(P2.Age);
+            Console.WriteLine(Person.Country);   // now the country is changed
+
+
+
+        }
+    }
+}
