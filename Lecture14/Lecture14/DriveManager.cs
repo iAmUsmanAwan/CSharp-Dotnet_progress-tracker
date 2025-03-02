d6f5848f210012c2afb4d0126a3aaf8cb6f21f3a
Admiral General <muhammadusmanawan88@gmail.com>
2025-03-02 23:33:01 +0500

    Lecture14 done

---
+++ Lecture14/Lecture14/DriveManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;    // when working on filesystem
+using static System.Console;
+
+
+namespace Lecture14
+{
+    internal class DriveManager
+    {
+        internal static void GetDriveInformation()
+        {
+            var drives = DriveInfo.GetDrives();
+            foreach (var drive in drives)
+            {
+                if (drive.IsReady) 
+                {
+                    WriteLine(drive.Name + " : " + 
+                    drive.DriveType + " : " +  // Fixed
+                    drive.TotalSize + " : " +  // size in bytes
+                    drive.DriveFormat + " : " +  // NTFS
+                    drive.AvailableFreeSpace );   // free space in bytes
+                }
+                else
+                {
+                    WriteLine(drive.Name + " : " + 
+                        drive.DriveType );
+                }
+            }
+        }
+    }
+}
---
+++ Lecture14/Lecture14/FileSystemManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;    // when working on filesystem
+using static System.Console;
+
+namespace Lecture14
+{
+    internal class FileSystemManager
+    {
+        static internal void GetFileSystemInfo()
+        {
+            // path seperaator is different for different operating systems, for windows back slash is used and for linus forward slash is used
+            WriteLine("{0} {1}", "Path.PathSeparator", Path.PathSeparator);    // Path.PathSeparator ;
+            WriteLine("{0} {1}", "Path.DirectorySeparatorChar", Path.DirectorySeparatorChar);   // DirectorySeparatorChar \
+            WriteLine("{0} {1}", "GetTempPath", Path.GetTempPath());   // C:\Users\DELL\AppData\Local\Temp\
+            
+            WriteLine("{0} {1}", "Directory.GetCurrentDirectory", Directory.GetCurrentDirectory());   // E:\MY_STUFF\web_practice\FullStackWebDev\Dotnet\Lecture14\Lecture14
+            WriteLine("{0} {1}", "Environment.CurrentDirectory", Environment.CurrentDirectory);     // E:\MY_STUFF\web_practice\FullStackWebDev\Dotnet\Lecture14\Lecture14
+            WriteLine("{0} {1}", "Environment.SystemDirectory", Environment.SystemDirectory);      // C:\WINDOWS\system32
+            WriteLine("{0} {1}", "Environment.OSVersion", Environment.OSVersion);     // Microsoft Windows NT 10.0.19045.0
+            WriteLine("{0} {1}", "Environment.ProcessorCount", Environment.ProcessorCount);    // 4
+
+
+
+
+
+
+        }
+
+    }
+}
