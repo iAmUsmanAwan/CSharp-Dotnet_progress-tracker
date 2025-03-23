f17a855d02b14fc6d255363f07f9e16448072dc8
Admiral General <muhammadusmanawan88@gmail.com>
2025-03-23 19:21:39 +0500

    Layout

---
+++ Lecture46/Program.cs
@@ -0,0 +1,34 @@
+namespace Lecture46
+{
+    public class Program
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
