c483c4853d628a39f07f064ede2578f82dcf8430
Admiral General <muhammadusmanawan88@gmail.com>
2025-03-27 23:59:53 +0500

    controler + view

---
+++ Lecture48/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lecture48.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public string Index()
+        //{
+        //    return "Hello from Index of Home Controller...";
+        //}
+        //public string Student()
+        //{
+        //    return "Hello from Student...";
+        //}
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult Student() 
+        {
+            return View();
+        }
+    }
+}
---
+++ Lecture48/Controllers/StudentController1.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lecture48.Controllers
+{
+    public class StudentController : Controller
+    {
+        //public string Index()
+        //{
+        //    return "This is a Student Controller...";    // Displaying a simple string message
+        //}
+
+        public ViewResult Index()
+        {
+            return View("Index");     // displaying a Html page created in Views/Student/Index.cshtml
+        }   
+    }
+}
