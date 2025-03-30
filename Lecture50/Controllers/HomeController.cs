1eff12fbf715dfe2f2aeea7a4fdbb03fac966d75
Admiral General <muhammadusmanawan88@gmail.com>
2025-03-30 11:17:31 +0500

    TagHelpers, View Students MVC

---
+++ Lecture50/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lecture50.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
