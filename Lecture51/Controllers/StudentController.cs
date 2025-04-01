27c4d9c49c19fae2dd0da0986fa879292c6ae901
Admiral General <muhammadusmanawan88@gmail.com>
2025-04-01 18:12:21 +0500

    Server-Side Validation

---
+++ Lecture51/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lecture50.Models;
+
+namespace Lecture50.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult StudentForm()
+        {
+                return View();
+        }
+
+        [HttpPost]
+        public ViewResult StudentForm(Student s)
+        {
+            if (ModelState.IsValid)
+            {
+                StudentRepository.AddStudent(s);
+                return View("Thankx", s);
+            }
+            else {
+                ModelState.AddModelError(String.Empty, "Please correct the Data");
+                return View();
+            }
+        }
+
+        public ViewResult ListStudents()
+        {
+            return View(StudentRepository.Students);
+        }
+
+
+    }
+}
