305e1dac0213d585e7e1c90d2b301963bc62a219
Admiral General <muhammadusmanawan88@gmail.com>
2025-04-02 12:02:23 +0500

    Bootstrap CRUD Operation MVC

---
+++ Lecture53/Controllers/StudentController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lecture50.Models;
+
+namespace Lecture50.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ViewResult Remove(int id)
+        {
+            Student s = StudentRepository.Students.Find(s => s.Id == id);
+            StudentRepository.Students.Remove(s);
+            return View("ListStudents", StudentRepository.Students);
+        }
+
+        public ViewResult Details(int id)
+        {
+            Student s = StudentRepository.Students.Find(s => s.Id == id);
+            return View("Thankx", s);
+        }
+
+        public ViewResult Edit(int id)
+        {
+            Student s = StudentRepository.Students.Find(s => s.Id == id);
+            return View("Edit", s);
+        }
+
+        [HttpPost]
+        public ViewResult Edit(Student s)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (Student std in StudentRepository.Students)
+                {
+                    if (std.Id == s.Id)
+                    {
+                        std.Name = s.Name;
+                        std.Age = s.Age;
+                        std.CGPA = s.CGPA;
+                        std.Semester = s.Semester;
+                        break;
+                    }
+                }
+                return View("ListStudents", StudentRepository.Students);
+            }
+            else
+            {
+                ModelState.AddModelError(String.Empty, "Please correct the Data");
+                return View();
+            }
+        }
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult StudentForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult StudentForm(Student s)
+        {
+            if (ModelState.IsValid)
+            {
+                StudentRepository.AddStudent(s);
+                return View("Thankx", s);
+            }
+            else
+            {
+                ModelState.AddModelError(String.Empty, "Please correct the Data");
+                return View();
+            }
+        }
+
+        public ViewResult ListStudents()
+        {
+            return View(StudentRepository.Students);
+        }
+
+
+    }
+}
---
+++ Lecture53/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Lecture50.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Name")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Age")]
+        [Range(1, 100)]
+        public int? Age { get; set; }
+
+        [Required(ErrorMessage = "Please Enter CGPA")]
+        public float? CGPA { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Semester")]
+        public string Semester { get; set; }
+    }
+}
