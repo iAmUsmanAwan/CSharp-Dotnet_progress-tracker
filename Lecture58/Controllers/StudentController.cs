82b3b5666c82b2f944ad5791044eddb62d116485
Admiral General <muhammadusmanawan88@gmail.com>
2025-04-06 23:57:18 +0500

    Intro to Web Services

---
+++ Lecture58/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Lecture58.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lecture58.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        StudentRepository repo;
+        public StudentController()
+        {
+            repo = new StudentRepository();
+        }
+
+        [HttpGet]
+        public List<Student> Get()
+        {
+            return repo.GetAllStudents();
+        }
+
+    }
+}
