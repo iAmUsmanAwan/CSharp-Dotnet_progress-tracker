0123013a0a0a8abdb5dcf17cc6539c53915e2798
Admiral General <muhammadusmanawan88@gmail.com>
2025-04-15 19:50:31 +0500

    Updated

--- WPF_EMS/EMS_DAL/obj/Debug/net8.0/EMS_DAL.AssemblyInfo.cs
+++ WPF_EMS/EMS_DAL/obj/Debug/net8.0/EMS_DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS_DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5901ab822c4d98b36930d26cc72b90c4f7930d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS_DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS_DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
