59aa152a9eb8fdb36827c0cfb15caebdc194309f
Admiral General <muhammadusmanawan88@gmail.com>
2025-04-16 19:57:59 +0500

    Linq Query

---
+++ Lecture63/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Lecture63
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new string[] { "Mustafa", "Ahmad", "Haider", "Ali", "Hassan", "Hussain", "Abbass", "Akbar" };
+
+            var query = names.Where(NameLongerThanFour);   // Defining a linq query using a method group
+
+            var query2 = names.Where(NameLongerThanFour).ToList();   // if we want to execute the query immediately, we can use ToList() method
+
+            foreach (var name in query)
+            {
+                Console.WriteLine(name);   // Using the linq query
+            }  
+
+            static bool NameLongerThanFour(string name)
+            {
+                return name.Length > 4;
+            } 
+
+        }
+    }
+}
