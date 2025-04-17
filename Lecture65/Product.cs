e63c5e67ece88079195c14fb598ca545f2eee67a
Admiral General <muhammadusmanawan88@gmail.com>
2025-04-18 00:40:10 +0500

    Linq _ csv file

---
+++ Lecture65/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Lecture65
+{
+    class Product
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public int Year { get; set; }
+
+    }
+}
