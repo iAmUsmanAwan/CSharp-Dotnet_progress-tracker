94241f09a4ceebf97d4eed58eddf0d53106fd887
Admiral General <muhammadusmanawan88@gmail.com>
2025-04-17 06:31:40 +0500

    Linq Lambda Expression

---
+++ Lecture64/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+namespace Lecture64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new string[] { "Mustafa", "Shuja", "Zia", "Ibrahim",
+                    "Ali", "Sabir","Aziz" };
+            
+            bool myValue = true;
+
+            //var query = names.Where(x => x.Length > 4).Where(n => n.StartsWith("S"));    /// to get names with length > 4 and starting with S
+
+            //var query = names.Where(x => x.Length > 4).Select(n=> n.Substring(0,3));   /// to get names with length > 4 and only first 3 characters
+
+            //var query = names.Where(x => x.Length > 4).Select(n => n.Substring(0, 3)).OrderBy(n=>n);     /// to get names with length > 4 and only first 3 characters and order them alphabetically
+
+            //var query = names.Where(x => x.Length > 4).Select(n => n.Substring(0, 3)).OrderByDescending(n => n);    /// to get names with length > 4 and only first 3 characters and order them in descending order
+
+            //var query = names.Where(x => x.Length > 4).Select(n => n).OrderBy(n => n).ThenBy(n=> n.Length);   /// to get names with length > 4 and order them alphabetically and then by length
+
+
+            /// We can also define the query in a multiple lines
+            //var query = names.Where(x => x.Length > 4);
+            //query = query.Select(n => n);
+            //query = query.OrderBy(n => n).ThenBy(n => n.Length);
+
+            var query = names.Where(x => x.Length > 4);
+
+            query = query.Select(n => n);
+
+            if (myValue == true)
+            {
+                query = query.Select(n => n.Substring(0, 4));
+                query = query.OrderByDescending(n => n).ThenBy(n => n.Length);
+            }
+            else
+            {
+                query = query.OrderBy(n => n).ThenBy(n => n.Length);
+            }
+
+            foreach (var n in query)
+            {
+                Console.WriteLine(n);
+            }
+        
+        
+        }
+
+    }
+}
