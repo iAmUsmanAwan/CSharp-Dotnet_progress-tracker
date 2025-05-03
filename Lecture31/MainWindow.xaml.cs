4ddc6cae261566b9aae2fb6f955af3202663eb70
Admiral General <muhammadusmanawan88@gmail.com>
2025-05-03 22:16:34 +0500

    Observeable collection, Interface

---
+++ Lecture31/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lecture31.Models;
+
+namespace Lecture31
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// if we make a list then this list will not be updated on runtime, so instead we will make a ObservableCollection
+        //List<User> userList = new List<User>();
+
+        // ObservableCollection is a collection that provides notifications when items get added, removed, or when the whole list is refreshed.
+        ObservableCollection<User> userList = new ObservableCollection<User>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            userList.Add(new User() { Name = "Ali", Age = 25 });
+            userList.Add(new User() { Name = "Abbass", Age = 25 });
+            userList.Add(new User() { Name = "Haider", Age = 25 });
+            lstUser.ItemsSource = userList;
+        }
+
+        private void BtnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            userList.Add(new User() { Name = "New User", Age = 39 });
+
+        }
+
+        private void btnUpdateUser_Click(object sender, RoutedEventArgs e)
+        {
+            if(lstUser.SelectedItem != null)
+            {
+                User selectedUser = (User)lstUser.SelectedItem;
+                selectedUser.Name = "Updated User";
+                selectedUser.Age = 30;
+            }
+        }
+
+        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            if(lstUser.SelectedItem != null)
+            {
+                userList.Remove((User)lstUser.SelectedItem);
+            }
+        }
+    }
+}
---
+++ Lecture31/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Lecture31.Models
+{
+    class User : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                NotifyPropertyChanged("Age");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
