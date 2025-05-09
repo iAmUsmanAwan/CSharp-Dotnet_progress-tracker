67c7b85584e88ef8ebf4f8dfe44059da950f77ca
Admiral General <muhammadusmanawan88@gmail.com>
2025-05-09 19:32:44 +0500

    IMultiValueConverter ||

---
+++ Lecture34/ConverterClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Lecture34
+{
+    public class ConverterClass : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Concat(values[0], ",", values[1]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            string? val = value as string;
+
+            if (string.IsNullOrEmpty(val))
+                return new object[] { "", "" };
+
+            return val.Split(',');
+        }
+
+    }
+}
