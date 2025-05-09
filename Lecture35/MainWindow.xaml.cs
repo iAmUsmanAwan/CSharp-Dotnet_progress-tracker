18be3c4baf18bcf9822b9fd4375827b05d4a1661
Admiral General <muhammadusmanawan88@gmail.com>
2025-05-10 02:01:49 +0500

    DataContext

---
+++ Lecture35/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture35
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Student s = new Student{ Name="Ali", Age=23, CGPA=3.5f };
+            this.DataContext = s;
+
+        }
+    }
+}
---
+++ Lecture35/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture35
+{
+    public class Student
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public float CGPA { get; set; }
+    }
+}
