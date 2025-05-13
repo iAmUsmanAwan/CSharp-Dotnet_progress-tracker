ca8c2d99a8b959cbecdc30abde66d64c77bc3089
Admiral General <muhammadusmanawan88@gmail.com>
2025-05-13 21:36:18 +0500

    WPF Commands

---
+++ Lecture36/MessageCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace Lecture36
+{
+    class MessageCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private MessageViewModel messagevm;
+
+        public MessageCommand(MessageViewModel vm)
+        {
+            this.messagevm = vm;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            // to maintain the state of the button
+            return true;
+            // if we want to disable the button then we will return false
+        }
+        public void Execute(object? parameter)
+        {
+            messagevm.DisplayMessage();
+        }
+    }
+    
+}
+
---
+++ Lecture36/MessageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lecture36
+{
+    class MessageViewModel
+    {
+        public string? MessageText { get; set; }
+        public MessageCommand cmd { get; set; }
+
+        public MessageViewModel()
+        {
+            cmd = new MessageCommand(this);
+        }
+
+        public void DisplayMessage()
+        {
+            MessageBox.Show(MessageText);
+        }   
+
+    }
+}
