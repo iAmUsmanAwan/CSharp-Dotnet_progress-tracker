deacad2041760e0f235df6f73d0bbb8eb50686cf
Admiral General <muhammadusmanawan88@gmail.com>
2025-05-14 20:14:46 +0500

    ICommand Parameter and CanExecuteChanged

---
+++ Lecture37/MessageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lecture37
+{
+    class MessageViewModel
+    {
+        
+        public MessageCommand cmd { get; set; }
+
+        public MessageViewModel()
+        {
+            cmd = new MessageCommand(this);
+        }
+
+        public bool canExecute(string? m)
+        {
+            if (string.IsNullOrEmpty(m))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        } 
+
+        public void DisplayMessage(string MessageText)
+        {
+            MessageBox.Show(MessageText);
+        }
+
+    }
+}
