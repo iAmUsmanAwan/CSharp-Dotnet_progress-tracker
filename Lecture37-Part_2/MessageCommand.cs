f5db78265d95763ffe2c034aefb97476232251ba
Admiral General <muhammadusmanawan88@gmail.com>
2025-05-15 23:39:24 +0500

    Multiple Parameters with Command in WPF

---
+++ Lecture37-Part_2/MessageCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lecture37_Part_2
+{
+    class MessageCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private MessageViewModel messagevm;
+
+        public MessageCommand(MessageViewModel vm)
+        {
+            this.messagevm = vm;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return messagevm.canExecute(parameter);
+        }
+        public void Execute(object? parameter)
+        {
+            messagevm.DisplayMessage(parameter);
+        }
+    }
+}
---
+++ Lecture37-Part_2/MessageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lecture37_Part_2
+{
+    class MessageViewModel
+    {
+
+        public MessageCommand cmd { get; set; }
+
+        public MessageViewModel()
+        {
+            cmd = new MessageCommand(this);
+        }
+
+        public bool canExecute(object m)
+        {
+            var data = m as object[];
+            var d1 = data[0] as string;
+            var d2 = data[1] as string;
+
+            if (string.IsNullOrEmpty(d1) || string.IsNullOrEmpty(d2))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void DisplayMessage(object m)
+        {
+            var data = m as object[];
+            var d1 = data[0] as string;
+            var d2 = data[1] as string;
+
+            MessageBox.Show($"{d1} and {d2}");
+        }
+
+    }
+}
