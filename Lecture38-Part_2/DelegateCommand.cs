d1e06414f881be636b304f32a7276a8b7be7fd30
Admiral General <muhammadusmanawan88@gmail.com>
2025-05-18 23:23:59 +0500

    Relay {Delegate} Commands

---
+++ Lecture38-Part_2/DelegateCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lecture38_Part_2
+{
+    /// this is also known as RelayCommand, by using delegate commands to simplify the code
+
+    class DelegateCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private readonly Action<string> _execute;
+        private readonly Predicate<string> _canExecute;
+
+        public DelegateCommand(Action<string> e, Predicate<string> c)
+        {
+            _execute = e;
+            _canExecute = c;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute(parameter as string ?? "");
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter as string ?? "");
+        }
+    }
+}
---
+++ Lecture38-Part_2/MessageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lecture38_Part_2
+{
+    class MessageViewModel
+    {
+
+        public DelegateCommand cmd { get; set; }
+        public DelegateCommand cmd2 { get; set; }
+
+        public MessageViewModel()
+        {
+            cmd = new DelegateCommand(DisplayMessage, canExecute);
+            cmd2 = new DelegateCommand(DisplayMessageOnConsole, canExecute);
+        }
+
+        public bool canExecute(object m)
+        {
+            if (string.IsNullOrEmpty(m as string))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void DisplayMessage(object m)
+        {
+            MessageBox.Show(m as string);
+        }
+
+        public void DisplayMessageOnConsole(object MessageText)
+        {
+            Console.WriteLine(MessageText);
+        }
+
+    }
+}
