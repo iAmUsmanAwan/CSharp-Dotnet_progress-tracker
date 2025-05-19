d7242e9b31bd104cc89d6cc04e578516cc87b04a
Admiral General <muhammadusmanawan88@gmail.com>
2025-05-19 23:50:35 +0500

    MVVM in WPF

---
+++ Lecture39/Models/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture39.Models
+{
+    class Student : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged("Id"); }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+
+    }
+}
---
+++ Lecture39/Models/StudentService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture39.Models
+{
+    class StudentService
+    {
+        ObservableCollection<Student> studentList; 
+        
+        public StudentService()
+        {
+            studentList = new ObservableCollection<Student>();
+            studentList.Add(new Student() { Id = 1, Name = "Muhammad", Age = 40 });
+            studentList.Add(new Student() { Id = 2, Name = "Ali", Age = 13 });
+            studentList.Add(new Student() { Id = 3, Name = "Hussain", Age = 10 });
+        }
+
+        public void AddStudent(Student s)
+        {
+            studentList.Add(s); 
+        }
+
+        public ObservableCollection<Student> GetAllStudents()
+        {
+            return studentList;
+        }
+
+    }
+}
