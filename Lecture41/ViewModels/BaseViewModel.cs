66bdbd528fede0a4a213b03a650b41389a00210f
Admiral General <muhammadusmanawan88@gmail.com>
2025-05-25 23:50:23 +0500

    Multiview App in WPF using MVVM

---
+++ Lecture41/ViewModels/BaseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture41.ViewModels
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propName)
+        {
+
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+
+        }
+    }
+}
---
+++ Lecture41/ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lecture41.Command;
+using System.Windows.Input;
+
+namespace Lecture41.ViewModels
+{
+    class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel selectedViewModel;
+
+        public BaseViewModel SelectedViewModel
+        {
+            get { return selectedViewModel; }
+            set { selectedViewModel = value; OnPropertyChanged("SelectedViewModel"); }
+        }
+
+        public ICommand UpdateViewCommand { get; set; }
+        public MainViewModel()
+        {
+            UpdateViewCommand = new DelegateCommand(ViewSelector, canExecute);
+            selectedViewModel = new HomeViewModel();
+        }
+
+        bool canExecute(object o)
+        {
+            return true;
+        }
+
+        void ViewSelector(object o)
+        {
+
+            if ((o as string).Equals("Student"))
+            {
+                SelectedViewModel = new StudentViewModel();
+            }
+            else if ((o as string).Equals("Course"))
+            {
+                SelectedViewModel = new CourseViewModel();
+            }
+            else if ((o as string).Equals("Home"))
+            {
+                SelectedViewModel = new HomeViewModel();
+            }
+        }
+    }
+}
